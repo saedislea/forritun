8545fcbb62eaa08d4918d5bd7af3343387225b08
Sædís Lea <saedislea@Sdiss-MacBook-Pro.local>
2018-09-04 10:45:05 +0000

    max_inint tilbuid

---
+++ max_init.py
@@ -0,0 +1,12 @@
+max_int = 0
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)    # Do not change this line
